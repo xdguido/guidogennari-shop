1eaacada718ad41feddd2febc42bbbadac75dc75
xdguido <guidogennari95@gmail.com>
2023-03-08 20:56:19 -0300

    feat: add skeleton loaders

--- components/Products/ProductsLayout/ProductsList.tsx
+++ components/Products/ProductsLayout/ProductsList.tsx
@@ -16,7 +16,27 @@ export default function ProductsList({ products, error, isLoading }: Props) {
         return <div>error</div>;
     }
     if (isLoading) {
-        return <div>loading</div>;
+        return (
+            <>
+                <div className="bg-base-100 mx-auto max-w-2xl p-4 sm:p-6 lg:max-w-7xl lg:p-8 animate-pulse">
+                    <h2 className="sr-only">Loading</h2>
+
+                    <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+                        {Array.from({ length: 16 }).map((_, index) => (
+                            <div key={index} className="group">
+                                <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded bg-base-200 xl:aspect-w-7 xl:aspect-h-8">
+                                    <div className="flex items-center justify-center">
+                                        {/* <CameraIcon className="h-11 w-11 group-hover:opacity-75" /> */}
+                                    </div>
+                                </div>
+                                <div className="mt-4 rounded bg-base-200 h-[20px]"></div>
+                                <div className="mt-1 rounded bg-base-200 h-[28px]"></div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </>
+        );
     }
     return (
         <>
@@ -26,7 +46,7 @@ export default function ProductsList({ products, error, isLoading }: Props) {
                 <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
                     {displayProducts.map((product) => (
                         <Link key={product.id} href="/" className="group">
-                            <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-base-200 xl:aspect-w-7 xl:aspect-h-8">
+                            <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded bg-base-200 xl:aspect-w-7 xl:aspect-h-8">
                                 {product.imageSrc ? (
                                     <Image
                                         src={product.imageSrc}
@@ -35,7 +55,9 @@ export default function ProductsList({ products, error, isLoading }: Props) {
                                         fill
                                     />
                                 ) : (
-                                    <CameraIcon className="group-hover:opacity-75" />
+                                    <div className="flex items-center justify-center">
+                                        <CameraIcon className="h-8 w-8 group-hover:opacity-75" />
+                                    </div>
                                 )}
                             </div>
                             <h3 className="mt-4 text-sm ">{product.name}</h3>
