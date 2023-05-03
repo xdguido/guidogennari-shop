20ed9f4a0ccf79b6eaa4b8a5cc39373f8ca43537
xdguido <guidogennari95@gmail.com>
2023-05-03 02:07:01 -0300

    feat: add endpoint

---
+++ pages/api/categories/index.ts
@@ -0,0 +1,16 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { createRouter } from 'next-connect';
+import { errorHandler, noMatchHandler } from '@lib/api/errorHandler';
+import getCategories from '@lib/getCategories';
+
+const router = createRouter<NextApiRequest, NextApiResponse>();
+
+router.get(async (req, res) => {
+    const data = await getCategories();
+    return res.status(200).json(data);
+});
+
+export default router.handler({
+    onError: errorHandler,
+    onNoMatch: noMatchHandler
+});
