f9533aacf5b250446d61f628327c316ae6e99486
xdguido <guidogennari95@gmail.com>
2023-05-05 03:11:31 -0300

    feat: update props

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -4,39 +4,28 @@ import clsx from 'clsx';
 type Props = {
     name: string;
     label: string;
-    placeholder?: string;
-    type: string;
-    register: object;
-    error: string | null;
+    register?: object;
+    error?: string | null;
     className?: string;
-};
-export default function Input({
-    name,
-    label,
-    placeholder,
-    type,
-    register,
-    error,
-    className
-}: Props) {
+} & React.HTMLProps<HTMLInputElement>;
+export default function Input({ name, label, register, error, className, ...props }: Props) {
     return (
         <div className="mb-2">
-            <label htmlFor={name} className="mb-1 block text-sm font-medium text-gray-600">
+            <label htmlFor={name} className="mb-1 block text-sm font-medium ">
                 {label}
             </label>
             <input
                 className={clsx([
-                    'form-control block w-full border border-solid bg-white bg-clip-padding px-4 py-2 font-normal text-gray-700 focus:ring-2',
-                    error ? 'border-red-300 ring ring-red-300' : 'border-gray-300',
-                    'm-0 rounded-md transition ease-in-out focus:border-sky-300 focus:bg-white focus:text-gray-700 focus:outline-none focus:ring-sky-300',
+                    'form-control block w-full border border-solid bg-base-100 bg-clip-padding px-4 py-2 font-normal focus:ring-2',
+                    error ? 'border-error' : 'border-neutral',
+                    'm-0 rounded-md transition ease-in-out focus:border-accent  focus:outline-none focus:ring-accent',
                     className
                 ])}
                 name={name}
-                placeholder={placeholder}
-                type={type}
                 {...register}
+                {...props}
             />
-            {error ? <p className="mt-2 text-xs text-red-500">{error}</p> : null}
+            {error ? <p className="mt-2 text-xs text-error">{error}</p> : null}
         </div>
     );
 }
