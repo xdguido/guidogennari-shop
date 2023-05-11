79293624d32befaa58e018ee8279c581c2c48168
xdguido <guidogennari95@gmail.com>
2023-05-11 17:23:15 -0300

    feat: update style

--- components/Products/PaginationButtons.tsx
+++ components/Products/PaginationButtons.tsx
@@ -1,5 +1,5 @@
+import Button from '@ui/Button';
 import clsx from 'clsx';
-import Link from 'next/link';
 type Props = {
     currentPageIndex: number;
     sort?: string;
@@ -15,44 +15,29 @@ export default function PaginationButtons({
     basePath
 }: Props) {
     return (
-        <div className={clsx('flex items-center justify-center bg-base-100 px-4 py-3 sm:px-6')}>
-            <div>
-                <nav className="isolate inline-flex -space-x-px" aria-label="Pagination">
-                    <Link
-                        href={`${basePath}/${category}/${sort}/${currentPageIndex - 1}`}
-                        className={clsx(
-                            'relative inline-flex items-center rounded-md border border-base-content  px-4 py-2 text-sm font-medium bg-base-100',
-                            currentPageIndex === 1
-                                ? 'text-neutral border-neutral pointer-events-none'
-                                : 'hover:bg-base-200'
-                        )}
-                    >
-                        Previous
-                    </Link>
-                    <div className="flex items-center px-4 gap-2">
-                        <span
-                            aria-current="page"
-                            className="rounded-md bg-base-200 px-4 py-2 text-sm font-semibold"
-                        >
-                            {currentPageIndex}
-                        </span>
-                        <span className="text-sm ">
-                            of <span className="font-medium">{maxPageIndex}</span>
-                        </span>
-                    </div>
-                    <Link
-                        href={`${basePath}/${category}/${sort}/${currentPageIndex + 1}`}
-                        className={clsx(
-                            'relative inline-flex items-center rounded-md border border-base-content  px-4 py-2 text-sm font-medium bg-base-100',
-                            currentPageIndex === maxPageIndex
-                                ? 'text-neutral border-neutral pointer-events-none'
-                                : 'hover:bg-base-200'
-                        )}
-                    >
-                        Next
-                    </Link>
-                </nav>
-            </div>
+        <div className={clsx('bg-base-100 px-4 py-3 sm:px-6')}>
+            <nav className="flex items-center justify-center " aria-label="Pagination">
+                <Button
+                    href={`${basePath}/${category}/${sort}/${currentPageIndex - 1}`}
+                    className={clsx('btn-outline btn-sm no-animation normal-case')}
+                >
+                    Previous
+                </Button>
+                <div className="flex items-center px-4 gap-2 text-sm">
+                    <span aria-current="page" className="font-semibold">
+                        {currentPageIndex}
+                    </span>
+                    <span>
+                        of <span className="font-medium">{maxPageIndex}</span>
+                    </span>
+                </div>
+                <Button
+                    href={`${basePath}/${category}/${sort}/${currentPageIndex + 1}`}
+                    className={clsx('btn-outline btn-sm no-animation normal-case')}
+                >
+                    Next
+                </Button>
+            </nav>
         </div>
     );
 }
