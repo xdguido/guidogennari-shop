c29369083b27ee97df2eb5b4c40707ec9638daa3
xdguido <guidogennari95@gmail.com>
2023-07-09 23:44:55 -0300

    feat: add track and indicator

--- components/Product/MobileCarousel.tsx
+++ components/Product/MobileCarousel.tsx
@@ -1,30 +1,42 @@
 import React, { useReducer } from 'react';
 import { useSwipeable } from 'react-swipeable';
 import Image from 'next/image';
-import { EllipsisHorizontalIcon } from '@heroicons/react/24/outline';
 
-type Direction = 'PREV' | 'NEXT';
+type Direction = 'PREV' | 'NEXT' | 'TRACK';
 
 interface CarouselState {
     pos: number;
-    dir: Direction;
+    px: number;
+    isTracking: boolean;
 }
 
-type CarouselAction = { type: Direction; numItems: number };
+type CarouselAction = { type: Direction; numItems?: number; px?: number };
 
 function reducer(state: CarouselState, action: CarouselAction): CarouselState {
+    const { pos } = state;
+    const { numItems, px } = action;
     switch (action.type) {
         case 'PREV':
             return {
                 ...state,
-                dir: 'PREV',
-                pos: state.pos === 0 ? action.numItems - 1 : state.pos - 1
+                pos: pos === 0 ? pos : pos - 1,
+                isTracking: false
             };
         case 'NEXT':
             return {
                 ...state,
-                dir: 'NEXT',
-                pos: state.pos === action.numItems - 1 ? 0 : state.pos + 1
+                pos: pos === numItems - 1 ? pos : pos + 1,
+                isTracking: false
+            };
+        case 'TRACK':
+            const reducedMotion = px / 4;
+            const isLastPosition = px < 0 && pos === numItems - 1;
+            const isFirstPosition = px > 0 && pos === 0;
+
+            return {
+                ...state,
+                px: isLastPosition || isFirstPosition ? reducedMotion : px,
+                isTracking: true
             };
         default:
             return state;
@@ -33,7 +45,8 @@ function reducer(state: CarouselState, action: CarouselAction): CarouselState {
 
 const getInitialState = (numItems: number): CarouselState => ({
     pos: numItems - 1,
-    dir: 'NEXT'
+    px: 0,
+    isTracking: false
 });
 
 export default function MobileCarousel() {
@@ -50,8 +63,14 @@ export default function MobileCarousel() {
         dispatch({ type: dir, numItems });
     };
 
+    const track = (px: number) => {
+        dispatch({ type: 'TRACK', px, numItems });
+    };
+
     const handlers = useSwipeable({
-        onSwiping: (e) => console.log(e),
+        onSwiping: (e) => {
+            track(e.deltaX);
+        },
         onSwipedLeft: () => slide('NEXT'),
         onSwipedRight: () => slide('PREV'),
         preventScrollOnSwipe: true,
@@ -60,28 +79,33 @@ export default function MobileCarousel() {
 
     return (
         <>
-            <div
-                {...handlers}
-                className="flex w-full overflow-hidden rounded-lg aspect-w-1 aspect-h-1"
-            >
+            <div {...handlers} className="flex w-full overflow-hidden aspect-w-1 aspect-h-1">
                 <div
-                    className={`flex transition-all duration-350`}
-                    style={{ transform: `translateX(-${state.pos * 100}%)` }}
+                    className={`flex ${state.isTracking ? '' : 'transition-all duration-300'}`}
+                    style={{
+                        transform: state.isTracking
+                            ? `translateX(-${state.pos * 100}%) translateX(${state.px}px)`
+                            : `translateX(-${state.pos * 100}%)`
+                    }}
                 >
                     {images.map((image, index) => (
                         <div key={index} className="relative shrink-0 basis-full">
-                            <Image
-                                // key={index}
-                                src={image}
-                                alt={`Slide ${index}`}
-                                fill
-                            />
+                            <Image src={image} alt={`Slide ${index}`} fill />
                         </div>
                     ))}
                 </div>
             </div>
-            <div className="flex justify-center">
-                <EllipsisHorizontalIcon aria-hidden="true" className="text-gray-400 w-10 h-10" />
+            <div className="flex justify-center p-2 gap-1">
+                {Array.from({ length: images.length }).map((_, index) => {
+                    return (
+                        <span
+                            key={index}
+                            className={`h-1.5 w-1.5 rounded-full ${
+                                state.pos === index ? 'bg-info' : 'bg-gray-400'
+                            } `}
+                        ></span>
+                    );
+                })}
             </div>
         </>
     );
